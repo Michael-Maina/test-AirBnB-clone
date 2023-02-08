aa33087c9a43b7926dd67a3f1086b3a382b0997f
Michael-Maina <michaelmaina02@gmail.com>
2023-02-08 17:12:05 +0300

    Task 7 partially done

--- console.py
+++ console.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 """Entry point for the command interpreter """
 import cmd
+import json
+from models.__init__ import storage
+from models.base_model import BaseModel
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter class for AirBnB program"""
 
     prompt = '(hbnb) '
+
+    __classes = {"BaseModel", "User", "State",
+                 "City", "Amenity", "Place", "Review"}
 
     def do_emptyline(self):
         """Executes nothing when no command is passed to the interpreter"""
@@ -21,15 +28,64 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the interpreter"""
         return True
 
-    def do_petty(self, arg):
-        """testing input processing"""
+    def do_create(self, arg):
+        """
+        Creates a new instance of a class, saves it and prints the id
+        """
         line = arg.split()
-        print("1. {} and 2. {}".format(line[0], line[1]))
+        if len(line) == 0:
+            print("** class name missing **")
+        else:
+            if not line[0] in self.__classes:
+                print("** class doesn't exist **")
+            else:
+                new_inst = eval(line[0])()
+                new_inst.save()
+                print("{}".format(new_inst.id))
 
-    def help_petty(self):
-        """Help output for petty command"""
-        print("This command is meant to test out a few examples")
+    def help_create(self):
+        """Help output for create command"""
+        print("Creates a new instance of a class, saves it and prints the id")
         print()
+
+    def do_show(self, arg):
+        """
+        Prints string representation of an instance based on class name and id
+        """
+        line = arg.split()
+        if len(line) == 0:
+            print("** class name missing **")
+        else:
+            if not line[0] in self.__classes:
+                print("** class doesn't exist **")
+            elif line[1] == "":
+                print("** instance id missing **")
+            else:
+                search_key = line[0] + "." + line[1]
+                check = False
+                for key, value in storage.all().items():
+                    if search_key == key:
+                        check = True
+                        print(value)
+                        break
+                if check == False:
+                    print("** no instance found **")
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances based or not on the class name
+        """
+        line = arg.split()
+        if len(line) == 0:
+            for key, value in storage.all().items():
+                print(value)
+        elif not line[0] in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            for key, value in storage.all().items():
+                cls_name = key.split(".")
+                if cls_name[0] == line[0]:
+                    print(value)
 
 
 if __name__ == '__main__':
